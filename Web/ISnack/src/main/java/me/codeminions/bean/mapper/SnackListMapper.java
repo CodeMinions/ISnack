11eaf4c2f1cc731cd95af4232b9c47aa837f7a16
阿伟 <845946286@qq.com>
2019-10-07 16:51:27 +0800

    Create SnackListMapper.java

---
+++ Web/ISnack/src/main/java/me/codeminions/bean/mapper/SnackListMapper.java
@@ -0,0 +1,9 @@
+package me.codeminions.bean.mapper;
+
+import me.codeminions.bean.db.SnackList;
+
+import java.util.List;
+
+public interface SnackListMapper {
+    public void setSnackList(SnackList snackList);     //发送零食清单
+}
