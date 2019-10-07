03b5d049dd0859dbc7add3c98276f87b32a3be45
阿伟 <845946286@qq.com>
2019-10-07 17:32:11 +0800

    Update SnackList.java

--- Web/ISnack/src/main/java/me/codeminions/bean/db/SnackList.java
+++ Web/ISnack/src/main/java/me/codeminions/bean/db/SnackList.java
@@ -28,11 +28,8 @@ public class SnackList {     //对应表t_snack_list
     @Expose
     private String time;
 
-    public SnackList(int listID,int list_id,int user_id,int snack_id,String title,String content){
-        this.listID=listID;
-        this.list_id=list_id;
+    public SnackList(int user_id,String title,String content){
         this.user_id=user_id;
-        this.snack_id=snack_id;
         this.title=title;
         this.content=content;
     }
